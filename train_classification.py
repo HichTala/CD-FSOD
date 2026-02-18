ff3405e51ea9a4cb300a99dec1372d56a1c5bb30
HichTala <98521878+HichTala@users.noreply.github.com>
2026-02-18 16:11:33 +0100

    Refactor training process and update dataset paths for improved organization and clarity

--- train_classification.py
+++ train_classification.py
@@ -52,6 +52,70 @@ class PreTrainingBackboneForImageClassification(nn.Module):
         return x
 
 
+def training_step(phase, model, dataloaders, optimizer, is_inception, criterion, best_acc, best_model_wts, output_dir, epoch, val_acc_history):
+    if phase == 'train':
+        model.train()  # Set model to training mode
+    else:
+        model.eval()  # Set model to evaluate mode
+
+    running_loss = 0.0
+    running_corrects = 0
+
+    # Iterate over data.
+    for sample in dataloaders[phase]:
+        inputs = sample["pixel_values"].to(device)
+        labels = sample["labels"].to(device)
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward
+        # track history if only in train
+        with torch.set_grad_enabled(phase == 'train'):
+            # Get model outputs and calculate loss
+            # Special case for inception because in training it has an auxiliary output. In train
+            #   mode we calculate the loss by summing the final output and the auxiliary output
+            #   but in testing we only consider the final output.
+            if is_inception and phase == 'train':
+                # From https://discuss.pytorch.org/t/how-to-optimize-inception-model-with-auxiliary-classifiers/7958
+                outputs, aux_outputs = model(inputs)
+                loss1 = criterion(outputs, labels)
+                loss2 = criterion(aux_outputs, labels)
+                loss = loss1 + 0.4 * loss2
+            else:
+                outputs = model(inputs)
+                loss = criterion(outputs, labels)
+
+            _, preds = torch.max(outputs, 1)
+
+            # backward + optimize only if in training phase
+            if phase == 'train':
+                loss.backward()
+                optimizer.step()
+
+        # statistics
+        running_loss += loss.item() * inputs.size(0)
+        running_corrects += torch.sum(preds == labels.data)
+
+    epoch_loss = running_loss / len(dataloaders[phase].dataset)
+    epoch_acc = running_corrects.double() / len(dataloaders[phase].dataset)
+
+    print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
+    wandb.log({f'{phase}_loss': epoch_loss, f'{phase}_acc': epoch_acc})
+
+    # deep copy the model
+    if phase == 'validation' and epoch_acc > best_acc:
+        best_acc = epoch_acc
+        best_model_wts = copy.deepcopy(model.state_dict())
+        os.makedirs(os.path.join("runs", output_dir), exist_ok=True)
+        torch.save(best_model_wts, os.path.join(f"runs/{output_dir}", f'resnet50_epoch_{epoch}.pth'))
+    if phase == 'validation':
+        val_acc_history.append(epoch_acc)
+    if phase == 'test':
+        best_acc = epoch_acc
+    return best_acc, best_model_wts
+
+
 def train_model(model, dataloaders, criterion, optimizer, num_epochs=25, is_inception=False, output_dir="default"):
     since = time.time()
 
@@ -65,75 +129,22 @@ def train_model(model, dataloaders, criterion, optimizer, num_epochs=25, is_ince
         print('-' * 10)
 
         # Each epoch has a training and validation phase
-        for phase in ['train']:
-            if phase == 'train':
-                model.train()  # Set model to training mode
-            else:
-                model.eval()  # Set model to evaluate mode
-
-            running_loss = 0.0
-            running_corrects = 0
-
-            # Iterate over data.
-            for sample in dataloaders[phase]:
-                inputs = sample["pixel_values"].to(device)
-                labels = sample["labels"].to(device)
-
-                # zero the parameter gradients
-                optimizer.zero_grad()
-
-                # forward
-                # track history if only in train
-                with torch.set_grad_enabled(phase == 'train'):
-                    # Get model outputs and calculate loss
-                    # Special case for inception because in training it has an auxiliary output. In train
-                    #   mode we calculate the loss by summing the final output and the auxiliary output
-                    #   but in testing we only consider the final output.
-                    if is_inception and phase == 'train':
-                        # From https://discuss.pytorch.org/t/how-to-optimize-inception-model-with-auxiliary-classifiers/7958
-                        outputs, aux_outputs = model(inputs)
-                        loss1 = criterion(outputs, labels)
-                        loss2 = criterion(aux_outputs, labels)
-                        loss = loss1 + 0.4 * loss2
-                    else:
-                        outputs = model(inputs)
-                        loss = criterion(outputs, labels)
-
-                    _, preds = torch.max(outputs, 1)
-
-                    # backward + optimize only if in training phase
-                    if phase == 'train':
-                        loss.backward()
-                        optimizer.step()
-
-                # statistics
-                running_loss += loss.item() * inputs.size(0)
-                running_corrects += torch.sum(preds == labels.data)
-
-            epoch_loss = running_loss / len(dataloaders[phase].dataset)
-            epoch_acc = running_corrects.double() / len(dataloaders[phase].dataset)
-
-            print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
-            wandb.log({f'{phase}_loss': epoch_loss, f'{phase}_acc': epoch_acc})
-
-            # deep copy the model
-            if epoch_acc > best_acc:
-                best_acc = epoch_acc
-                best_model_wts = copy.deepcopy(model.state_dict())
-                os.makedirs(os.path.join("runs", output_dir), exist_ok=True)
-                torch.save(best_model_wts, os.path.join(f"runs/{output_dir}", f'resnet50_epoch_{epoch}.pth'))
-            if phase == 'validation':
-                val_acc_history.append(epoch_acc)
+        for phase in ['train', 'validation']:
+            best_acc, best_model_wts = training_step(phase, model, dataloaders, optimizer, is_inception, criterion, best_acc, best_model_wts, output_dir, epoch, val_acc_history)
 
         print()
+
 
     time_elapsed = time.time() - since
     print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
     print('Best val Acc: {:4f}'.format(best_acc))
-
+    print()
+    print('Test Acc: {:4f}'.format(best_acc))
     # load best model weights
     model.load_state_dict(best_model_wts)
     torch.save(best_model_wts, os.path.join("runs", 'best_model.pth'))
+    print('Testing')
+    best_acc = training_step('test', model, dataloaders, optimizer, is_inception, criterion, best_acc, output_dir, epoch, val_acc_history)
     return model, val_acc_history
 
 
@@ -152,7 +163,7 @@ def transforms_fn(example_batch, split):
 
 
 def main(cfg, dataset_name, shot, seed):
-    dataset_path = f"/lustre/fsn1/projects/rech/mvq/ubc18yy/datasets/crop_datasets/{dataset_name}_{shot}shot_{seed}"
+    dataset_path = f"HichTala/{dataset_name}_{shot}shot_{seed}"
     dataset = load_dataset(dataset_path)
     num_classes = len(dataset["train"].features["label"].names)
 
@@ -177,22 +188,24 @@ def main(cfg, dataset_name, shot, seed):
             print("\t", name)
 
     # Observe that all parameters are being optimized
-    optimizer = optim.SGD(params_to_update, lr=0.00001, momentum=0.9)
+    optimizer = optim.SGD(params_to_update, lr=0.0001, momentum=0.9)
 
     print("Initializing Datasets and Dataloaders...")
 
     dataset["train"].set_transform(partial(transforms_fn, split="train"))
-    # dataset["validation"].set_transform(partial(transforms_fn, split="validation"))
+    dataset["validation"].set_transform(partial(transforms_fn, split="validation"))
+    dataset["test"].set_transform(partial(transforms_fn, split="test"))
 
     dataloaders = {
-        split_name: torch.utils.data.DataLoader(dataset[split_name], batch_size=256, shuffle=True, num_workers=3,
+        split_name: torch.utils.data.DataLoader(dataset[split_name], batch_size=32, shuffle=True, num_workers=3,
                                                 collate_fn=collate_fn)
-        for split_name in ['train']
+        for split_name in ['train', 'validation', 'test']
     }
 
     criterion = nn.CrossEntropyLoss()
 
-    train_model(model, dataloaders, criterion, optimizer, num_epochs=150, output_dir=f"{dataset_name}_{shot}shot_{seed}")
+    train_model(model, dataloaders, criterion, optimizer, num_epochs=30,
+                output_dir=f"{dataset_name}_{shot}shot_{seed}")
 
 
 if __name__ == '__main__':
@@ -201,6 +214,8 @@ if __name__ == '__main__':
     parser.add_argument('--shot', required=True, help='dataset to use for training')
     parser.add_argument('--seed', required=True, help='dataset to use for training')
     args = parser.parse_args()
+
+    torch.manual_seed(args.seed)
 
     input_size = 224
 
@@ -217,6 +232,12 @@ if __name__ == '__main__':
             transforms.ToTensor(),
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
         ]),
+        'test': transforms.Compose([
+            transforms.Resize(input_size),
+            transforms.CenterCrop(input_size),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
     }
 
     cfg = get_cfg()
@@ -225,6 +246,6 @@ if __name__ == '__main__':
     cfg.merge_from_list([])
     cfg.freeze()
 
-    wandb.init(project=f"ICIP-2026_{args.dataset}", group=f"{args.shot}_shots")
+    wandb.init(project=f"ICIP-2026-cdfsod_{args.dataset}", group=f"{args.shot}_shots")
 
     main(cfg, dataset_name=args.dataset, shot=args.shot, seed=args.seed)
